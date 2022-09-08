12e1ced52cc1431eda2cbc188a6b7fa5f7355aed
quannguyen1012 <110601149+quannguyen1012@users.noreply.github.com>
2022-09-08 13:48:26 +0700

    Add files via upload

---
+++ src/MinArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size;
+        int[] arr;
+
+        do {
+            System.out.print("Enter size: ");
+            size = sc.nextInt();
+            if(size > 20) {
+                System.out.println("size not be less than 20");
+            }
+        }while (size > 20);
+
+        arr = new int[size];
+        for (int i=0; i < size ; i++) {
+            System.out.print("Enter element : ");
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i=0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+
+        System.out.println();//xuống dòng;
+
+        int max = arr[0];
+        for (int i=1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The largest element in the array is: " + max);
+    }
+}
