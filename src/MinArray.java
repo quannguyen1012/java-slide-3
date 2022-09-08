e325a8a53a1ceafe711494e1b6bc8ff682137dfc
quannguyen1012 <110601149+quannguyen1012@users.noreply.github.com>
2022-09-08 14:03:22 +0700

    Add files via upload

--- src/MinArray.java
+++ src/MinArray.java
@@ -3,31 +3,14 @@ import java.util.Scanner;
 public class MinArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size;
-        int[] arr;
 
-        do {
-            System.out.print("Enter size: ");
-            size = sc.nextInt();
-            if(size > 20) {
-                System.out.println("size not be less than 20");
-            }
-        }while (size > 20);
+        int arr[] = {1,2,5,7,8,9};
 
-        arr = new int[size];
-        for (int i=0; i < size ; i++) {
-            System.out.print("Enter element : ");
-            arr[i] = sc.nextInt();
-        }
-
-        for (int i=0; i < arr.length; i++) {
-            System.out.print(arr[i] + "\t");
-        }
-
-        System.out.println();//xuống dòng;
-
+        MinArray(arr);
+    }
+    public static void MinArray(int[] arr) {
         int min = arr[0];
-        for (int i=1; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (min > arr[i]) {
                 min = arr[i];
             }
